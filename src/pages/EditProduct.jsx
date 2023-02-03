5f083c04b090d703dc4a197b3d2edb023ec99fb1
suraj <surajmishra9318@gmail.com>
2023-02-03 19:14:46 +0530

    editProductStateSet

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
 const EditProduct = () => {
+
+    const initialState = {
+        name : '',
+        price : null,
+        category : '',
+        company : '',
+        userId : ''
+    }
+    const [data,setData] = useState(initialState)
+        var { id } = useParams();
+    
+    const getProductById = async() =>{
+        const req = await fetch(`http://localhost:5000/getProductById/${id}`,{
+            method : 'GET',
+        })
+        const response =await req.json();
+        console.log(response.length)
+        if(response.length > 0){
+            const [data] = response;
+            setData(data);
+        }
+        else{
+            toast.error('something went wrong')
+        }
+        
+    }
+        useEffect(()=>{
+            getProductById();
+        },[])
+   
   return (
     <div>
       <form className='form'>
@@ -8,22 +40,23 @@ const EditProduct = () => {
              <input type="hidden" />
             <div className="from-group">
                 <label htmlFor='name'>Name</label>
-                <input className="form-control"  placeholder="Enter product's name" type="text"  name="" id="name" />
+                <input className="form-control" value={data.name} onChange={(e)=>setData({...data,name:e.target.value})} placeholder="Enter product's name" type="text"  name="" id="name" />
             </div>
 
             <div className="from-group">
                 <label htmlFor='price'>Price</label>
-                <input className="form-control" type="number"  placeholder="Enter product's price"  name="" id="price" />
+                <input className="form-control" type="number" value={data.price} onChange={(e)=>setData({...data,price:e.target.value})}  placeholder="Enter product's price"  name="" id="price" />
             </div>
 
             <div className="from-group">
                 <label htmlFor='category'>Category</label>
-                <input className="form-control" type="text" placeholder="Enter category of product"  name="" id="category" />
+                <input className="form-control" type="text" value={data.category} onChange={(e)=>setData({...data,category:e.target.value})} placeholder="Enter category of product"  name="" id="category" />
             </div>
 
             <div className="from-group">
                 <label htmlFor='category'>Company</label>
-                <input className="form-control" type="text"  placeholder="Enter company name" name="" id="category" />
+                <input className="form-control" type="text" value={data.company} onChange={(e)=>setData({...data,company:e.target.value})} placeholder="Enter company name" name="" id="category" />
+                <input type="hidden" value={data._id} />
             </div>
             <button type='button' className='btn' >Update</button>
         </form>
